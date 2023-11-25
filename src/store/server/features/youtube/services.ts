846769cb59d3b96cf755548f99d6b56b9a657dc5
haesim <anjin309@gmail.com>
2023-11-25 14:57:17 +0900

    feat: google oauth, youtube api (#5)
    Co-authored-by: Dean <cheoljin.an@lotte.net>

---
+++ src/store/server/features/youtube/services.ts
@@ -0,0 +1,16 @@
+import ApiClient from '../../common/common';
+
+const getPlaylist = async (): Promise<YoutubePlaylistResponse> => {
+  const response = await ApiClient.get('/playlist');
+  return response.data;
+};
+
+const getPlaylistItem = async (
+  playlistId: string
+): Promise<YoutubePlaylistItemListResponse> => {
+  // return ApiClient.get(`/playlist/${playlistId}`);
+  const response = await ApiClient.get(`/playlist/${playlistId}`);
+  return response.data;
+};
+
+export { getPlaylist, getPlaylistItem };
---
+++ src/types/interfaces/youtube/playlist.d.ts
@@ -0,0 +1,48 @@
+interface YoutubeThumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface YoutubeSnippetLocalized {
+  description: string;
+  title: string;
+}
+
+interface YoutubeSnippet {
+  channelId: string;
+  channelTitle: string;
+  description: string;
+  localized: YoutubeSnippetLocalized;
+  title: string;
+  publishedAt: string;
+  thumbnails: {
+    default: YoutubeThumbnail;
+    high: YoutubeThumbnail;
+    maxres: YoutubeThumbnail;
+    medium: YoutubeThumbnail;
+    standard: YoutubeThumbnail;
+  };
+}
+
+interface YoutubeContentDetails {
+  itemCount: number;
+}
+
+interface YoutubePlaylistItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: YoutubeSnippet;
+  contentDetails: YoutubeContentDetails;
+}
+
+interface YoutubePlaylistResponse {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: YoutubePlaylistItem[];
+}
