32153119eceb1c8e03b04d745ca8d5069ee2346b
Dean <cheoljin.an@lotte.net>
2023-11-29 16:29:45 +0900

    fix: 디자인 개선

--- src/components/organisms/Drawer/index.tsx
+++ src/components/organisms/Drawer/index.tsx
@@ -8,12 +8,10 @@ import {
   Toolbar,
 } from '@mui/material';
 import type { FC } from 'react';
-import { useState } from 'react';
+import React from 'react';
 
 import Profile from '@/components/molecules/Profile';
 import { useAppRouting } from '@/hooks/useAppRouting';
-
-import AreaForDrawerOpenByHover from './index.styled';
 
 type MenuItems = {
   name: string;
@@ -24,67 +22,57 @@ type MenuItems = {
 
 interface IDrawerProps {
   menuItemList: MenuItems[];
+  open: boolean; // Receive open state as a prop
+  onClose: () => void; // Receive a function to close the drawer
 }
 
-const DrawerMenu: FC<IDrawerProps> = ({ menuItemList }) => {
+const DrawerMenu: FC<IDrawerProps> = ({ menuItemList, open, onClose }) => {
   const { currentMenu, navigateToMenu } = useAppRouting();
-  const [open, setOpen] = useState<boolean>(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   return (
-    <>
-      <Drawer
-        sx={{
+    <Drawer
+      sx={{
+        width: 240,
+        flexShrink: 0,
+        zIndex: 1199,
+        '& .MuiDrawer-paper': {
           width: 240,
-          flexShrink: 0,
-          zIndex: 1199,
-          '& .MuiDrawer-paper': {
-            width: 240,
-            boxSizing: 'border-box',
-            transition: 'width 0.2s ease-in-out',
-          },
+          boxSizing: 'border-box',
+          transition: 'width 0.2s ease-in-out',
+        },
+      }}
+      variant="temporary"
+      anchor="left"
+      open={open}
+      onClose={onClose}
+    >
+      <Toolbar
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '16px',
         }}
-        variant="temporary"
-        anchor="left"
-        open={open}
-        onClose={handleDrawerClose}
       >
-        <Toolbar
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: '16px',
-          }}
-        >
-          <Profile />
-        </Toolbar>
-        <Divider />
-        <List>
-          {menuItemList.map((item) => (
-            <ListItemButton
-              key={item.name}
-              selected={currentMenu.value === item.value}
-              onClick={() => {
-                navigateToMenu(item.value);
-                handleDrawerClose();
-              }}
-            >
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.name} />
-            </ListItemButton>
-          ))}
-        </List>
-      </Drawer>
-      <AreaForDrawerOpenByHover onMouseEnter={handleDrawerOpen} />
-    </>
+        <Profile />
+      </Toolbar>
+      <Divider />
+      <List>
+        {menuItemList.map((item) => (
+          <ListItemButton
+            key={item.name}
+            selected={currentMenu.value === item.value}
+            onClick={() => {
+              navigateToMenu(item.value);
+              onClose(); // Close the drawer when a menu item is clicked
+            }}
+          >
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.name} />
+          </ListItemButton>
+        ))}
+      </List>
+    </Drawer>
   );
 };
 
