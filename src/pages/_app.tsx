2777160bb658f22d82f6df2a08fb07e17d46e9d1
Dean <cheoljin.an@lotte.net>
2023-12-28 18:00:08 +0900

    feat: clarity by ms

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -172,6 +172,19 @@ const MyApp = (props: MyAppProps) => {
         `,
         }}
       />
+      <Script
+        id="clarity-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "kcwiyw3jb9");
+        `,
+        }}
+      />
       <SessionProvider session={pageProps.session}>
         <QueryClientProvider client={queryClient}>
           <QuizDatabaseProvider>
